db18abf42600a1abd73f94ebb4d29e167295621f
rus <ruslan.cheremisin@gmail.com>
2026-04-02 22:38:17 +1000

    Concurrency - ConcurrentBank

--- src/main/java/rus/cheremisin/Main.java
+++ src/main/java/rus/cheremisin/Main.java
@@ -1,8 +1,10 @@
 package rus.cheremisin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,22 +17,19 @@ public class Main {
         // Перевод между счетами
         Thread transferThread1 = new Thread(() -> bank.transfer(account1, account2, 200));
         Thread transferThread2 = new Thread(() -> bank.transfer(account2, account1, 100));
-        Thread transferThread3 = new Thread(() -> bank.transfer(null, account1, 100));
-        Thread transferThread4 = new Thread(() -> bank.transfer(account2, null, 100));
-        Thread transferThread5 = new Thread(() -> bank.transfer(account2, null, 0));
+        Thread transferThread3 = new Thread(() -> bank.transfer(account1, account2, 100));
+        Thread transferThread4 = new Thread(() -> bank.transfer(account2, account1, 100));
 
         transferThread1.start();
         transferThread2.start();
         transferThread3.start();
         transferThread4.start();
-        transferThread5.start();
 
         try {
             transferThread1.join();
             transferThread2.join();
             transferThread3.join();
             transferThread4.join();
-            transferThread5.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -42,41 +41,48 @@ public class Main {
 
 class ConcurrentBank {
 
-    private List<BankAccount> accountList = new ArrayList<>();
+    private Queue<BankAccount> accounts = new LinkedBlockingQueue<>();
 
     public BankAccount createAccount(int initialAmount) {
         BankAccount newAccount = new BankAccount(initialAmount);
-        accountList.add(newAccount);
+        accounts.add(newAccount);
         return newAccount;
     }
 
     public void transfer(BankAccount account1, BankAccount account2, int amount) {
-        if (amount > 0) {
-            System.out.println("Checking accounts info...");
-            checkAccountOnNull(account1);
-            System.out.println("Account sender is OK.");
-            checkAccountOnNull(account2);
-            System.out.println("Account receiver is OK.");
-            int account1Balance = account1.getBalance();
-            if (account1Balance > amount) {
-                account1.setBalance(amount * -1);
-                System.out.println("Decreased balance of sender");
-            } else {
-                throw new RuntimeException("Insufficient money on account-sender!");
+
+        BankAccount first = account1.hashCode() < account2.hashCode() ? account1 : account2;
+        BankAccount second = first == account2 ? account1 : account2;
+
+        synchronized (first) {
+            synchronized (second) {
+                if (amount > 0) {
+                    System.out.println("Checking accounts info...");
+                    checkAccountOnNull(account1);
+                    System.out.println("Account sender is OK. Balance is " + account1.getBalance());
+                    int account1Balance = account1.getBalance();
+                    if (account1Balance > amount) {
+                        account1.setBalance(amount * -1);
+                        System.out.println("Decreased balance of sender. Account ID #" + account1.getId() + "(-" + amount + ").  Balance is " + account1.getBalance());
+                    } else {
+                        throw new RuntimeException("Insufficient money on account-sender!");
+                    }
+                } else {
+                    throw new RuntimeException("Impossible to transfer 0 and less!");
+                }
+
+                checkAccountOnNull(account2);
+                System.out.println("Account receiver is OK. Balance is " + account2.getBalance());
+                account2.setBalance(amount);
+                System.out.println("Increased balance of receiver. Account ID #" + account1.getId() + "(+" + amount + "). Balance is " + account2.getBalance());
             }
-            account2.setBalance(amount);
-            System.out.println("Increased balance of sender");
-        } else {
-            throw new RuntimeException("Impossible to transfer 0 and less!");
         }
-
-
 
     }
 
     public int getTotalBalance() {
         AtomicInteger totalBalance = new AtomicInteger(0);
-        accountList.forEach(acc -> totalBalance.addAndGet(acc.getBalance()));
+        accounts.forEach(acc -> totalBalance.addAndGet(acc.getBalance()));
         return totalBalance.get();
     }
 
@@ -88,17 +94,39 @@ class ConcurrentBank {
 }
 
 class BankAccount {
+    private static long idCounter;
+    private long id;
     private AtomicInteger balance;
 
+//    private final ReentrantLock lock = new ReentrantLock();
+
     public BankAccount(int balance) {
+        this.id = idCounter++;
         this.balance = new AtomicInteger(balance);
     }
 
-    public synchronized void setBalance(int amount) {
+    public void setBalance(int amount) {
         this.balance.addAndGet(amount);
     }
 
     public int getBalance() {
         return balance.get();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BankAccount that = (BankAccount) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Objects.hashCode(id);
     }
 }
