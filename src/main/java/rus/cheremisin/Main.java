db61f690cd8a0cc5b3a047f8aad4e94612646326
rus <ruslan.cheremisin@gmail.com>
2026-04-06 11:45:26 +1000

    Stream API - aggregation and joining of results

--- src/main/java/rus/cheremisin/Main.java
+++ src/main/java/rus/cheremisin/Main.java
@@ -1,9 +1,41 @@
 package rus.cheremisin;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+
+        Map<String, Integer> averageGrades = students
+                .stream()
+                .map(Student::getGrades)
+                .parallel()
+                .flatMap(map -> map.entrySet()
+                        .stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (g1, g2) -> (g1 + g2)/2));
+        System.out.println(averageGrades);
+    }
+
+}
+
+class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
     }
 }
